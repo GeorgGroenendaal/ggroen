363ff4990586b260a2020f25827dd8df01f886bf
Georg Groenendaal <georg.groenendaal@gmail.com>
2023-02-10 16:38:38 +0000

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Alert, FooterLink } from 'flowbite-svelte';
+</script>
+
+<div class="p-8">
+	<Alert>
+		<span class="font-medium">Info alert!</span> Change a few s up and try submitting again.
+	</Alert>
+
+	<a href="/about">to about</a>
+</div>
